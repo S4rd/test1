7b37bcc86293ae1fef3e7d14e725a2e45eedd969
Samil <naslam@windowslive.com>
2022-05-11 20:58:22 +0300

    last changed

--- src/xx1.java
+++ src/xx1.java
@@ -2,5 +2,6 @@ public class xx1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(2+3);
+        System.out.println(3+4);
     }
 }
